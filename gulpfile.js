8cf65b6f064389fb701c22474824e2c5615c32a7
Richard Santos <rsantos@tray.com.br>
2014-09-24 09:56:56 -0300

    Added js task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
+/**
+ * Gulpfile
+ * @author Richard Santos <rsantos@tray.com.br>
+ */
+
 var gulp = require("gulp"),
   sass = require("gulp-sass"),
   concat = require("gulp-concat"),
   minifyCSS = require("gulp-minify-css"),
+  uglify = require("gulp-uglify"),
   path = require("path");
 
-var SASSPATH, CSSPATH;
+var SASSPATH, CSSPATH, JSPATH;
 
 gulp.task('sass', function(){
   console.log(SASSPATH);
@@ -16,10 +22,23 @@ gulp.task('sass', function(){
     .pipe(gulp.dest(CSSPATH));
 });
 
-var watcher = gulp.watch('**/*.scss', ['sass']);
-watcher.on("change", function(event) {
+gulp.task('js', function(){
+  gulp.src(JSPATH + "/*.js")
+    .pipe(concat("theme.min.js"))
+    .pipe(uglify({"compress": false}))
+    .pipe(gulp.dest(path.resolve(JSPATH, "../")));
+});
+
+// watchers
+var cssWatcher = gulp.watch('**/*.scss', ['sass']);
+cssWatcher.on("change", function(event) {
   SASSPATH = path.resolve(event.path, "../");
   CSSPATH  = path.resolve(event.path, "../../");
+});
+
+var jsWatcher = gulp.watch('**/js/modules/*.js', ['js']);
+jsWatcher.on("change", function(event) {
+  JSPATH = path.resolve(event.path, "../");
 });
 
 gulp.task("default");
